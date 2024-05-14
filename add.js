93f5cfb042c0149e6a5cedcabbe907ce08356f02
DLBell217 <157739853+DLBell217@users.noreply.github.com>
2024-05-14 11:06:15 -0400

    Add files via upload

---
+++ add.js
@@ -0,0 +1,34 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://Admin:Admin@firstcluster.xdrvkoh.mongodb.net/?retryWrites=true&w=majority&appName=firstCluster";
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+    try {
+      const database = client.db('students');
+      const collection = database.collection('student');
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+      for (let i =0; i<15; i++)
+        await collection.insertOne({ "name": "Dude", "course": "Doob", "roll_no": 21});
+    } catch (err) {
+        console.error('Error connecting to MongoDB', err);
+    }
+    client.close();
+  }
+  
+  run().catch(console.dir);
